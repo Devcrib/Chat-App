7713e5589c1c7a6bb1430473358fccd46dcf2225
Oluwakemi Fasae <kemifasae@yahoo.co.uk>
2018-01-22 17:29:03 +0100

    include functionalities

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+var port = 3700;
+
+
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+var io = require('socket.io').listen(app.listen(port));
+
+io.sockets.on('connection', function (socket) {
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+console.log("Listening on port " + port);
